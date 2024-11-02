c3f1d7b664ba940f08c40f8ed590c59a63f615a3
maryamsalamatl <maryamsalamat47@gmail.com>
2024-11-02 09:31:10 +0400

    refactor: style

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   const moveBack = useMoveBack();
 
   return (
-    <div className="sm:max-w-screen-sm pt-10 space-y-6">
+    <div className="container sm:max-w-screen-sm pt-10 space-y-6">
       <h1 className="font bold text-xl text-secondary-700">
         صفحه مورد نظر یافت نشد.
       </h1>
