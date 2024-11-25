b0b03773937df127ee05bc9a9376ae2bd04767c7
maryamsalamatl <maryamsalamat47@gmail.com>
2024-11-25 13:35:15 +0400

    refactor: upgrade protected routes to handle user verification

--- src/features/authentication/useAuthorize.js
+++ src/features/authentication/useAuthorize.js
@@ -8,8 +8,14 @@ export default function useAuthorize() {
   let isAuthenticated = false;
   if (user) isAuthenticated = true;
 
+  let isVerified = false;
+  if (user && Number(user.status) === 2) isVerified = true;
+
+  let isRejected = false;
+  if (user && Number(user.status === 0)) isRejected = true;
+
   let isAuthorized = false;
   if (pathname.includes(user?.role.toLowerCase())) isAuthorized = true;
 
-  return { isAuthenticated, isAuthorized, isLoading };
+  return { isAuthenticated, isAuthorized, isLoading, isVerified, isRejected };
 }
--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -2,15 +2,32 @@ import { useEffect } from "react";
 import useAuthorize from "../features/authentication/useAuthorize";
 import { useNavigate } from "react-router-dom";
 import Loading from "./Loading";
+import toast from "react-hot-toast";
 
 export default function ProtectedRoute({ children }) {
-  const { isAuthenticated, isAuthorized, isLoading } = useAuthorize();
+  const { isAuthenticated, isAuthorized, isLoading, isVerified, isRejected } =
+    useAuthorize();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!isAuthenticated && !isLoading) navigate("/auth");
+    if (isRejected && !isLoading) {
+      toast("پروفایل شما توسط ادمین رد شده.");
+      return navigate("/");
+    }
+    if (!isVerified && !isLoading) {
+      toast("پروفایل شما در انتظار تایید است");
+      return navigate("/");
+    }
     if (!isAuthorized && !isLoading) navigate("/not-access", { replace: true });
-  }, [isAuthenticated, isAuthorized, isLoading, navigate]);
+  }, [
+    isAuthenticated,
+    isAuthorized,
+    isLoading,
+    navigate,
+    isVerified,
+    isRejected,
+  ]);
 
   if (isLoading)
     return (
