a916cb7adf7cbdb1f8d744c05d4448cf1b696438
maryamsalamatl <maryamsalamat47@gmail.com>
2024-11-30 11:20:28 +0400

    refactor: image folder

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -7,7 +7,7 @@ export default function UserAvatar() {
     <div className="flex items-center gap-x-2">
       <img
         className="w-5 h-5 sm:w-7 sm:h-7 rounded-full border-2 border-secondary-500 object-contain object-center"
-        src={"/profile-icon.png"}
+        src={"/images/profile-icon.png"}
         alt="user-profile"
       />
       <p className="basic__fontSize text-secondary-700">{user?.name}</p>
