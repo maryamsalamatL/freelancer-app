3c8f34503fe46448aeeabb96443bcf8163374b4b
maryamsalamatl <maryamsalamat47@gmail.com>
2024-12-28 21:45:10 +0400

    refactor: home link instead of unnecessary dashboard link

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -1,4 +1,4 @@
-import { HiOutlineUser, HiSun } from "react-icons/hi";
+import { HiHome } from "react-icons/hi";
 import { Link } from "react-router-dom";
 import Logout from "../features/authentication/Logout";
 import DarkModeToggle from "./DarkModeToggle";
@@ -6,13 +6,13 @@ import DarkModeToggle from "./DarkModeToggle";
 export default function HeaderMenu() {
   return (
     <ul className="flex items-center gap-x-4">
-      <li>
-        <Link to={"dashboard"}>
-          <HiOutlineUser className="icon text-primary-900" />
-        </Link>
-      </li>
       <li className="flex">
         <DarkModeToggle />
+      </li>
+      <li>
+        <Link to={"/"}>
+          <HiHome className="icon text-primary-900" />
+        </Link>
       </li>
       <li className="flex">
         <Logout />
